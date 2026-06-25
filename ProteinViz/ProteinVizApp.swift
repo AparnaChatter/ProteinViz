57d461d0645340a5bca7baa619cc0acc7e29792c
AparnaChatterjee <chatterjeeaparna007@gmail.com>
2026-06-24 19:45:18 -0700

    Initial Commit

---
+++ ProteinViz/ProteinVizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProteinVizApp.swift
+//  ProteinViz
+//
+//  Created by Scholar on 6/24/26.
+//
+
+import SwiftUI
+
+@main
+struct ProteinVizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProteinVizTests/ProteinVizTests.swift
@@ -0,0 +1,18 @@
+//
+//  ProteinVizTests.swift
+//  ProteinVizTests
+//
+//  Created by Scholar on 6/24/26.
+//
+
+import Testing
+
+struct ProteinVizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
